40e3679a314f2c9bde4a602c5422ddeb97f6e1bf
Prateek Pandey <prateekpandey@Maverick.local>
2021-12-02 12:58:39 +0530

    adding debug logs

--- src/view/components/todo/ToDo.jsx
+++ src/view/components/todo/ToDo.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
-let noteId = 0;
+let uniqueNoteId = 0;
 
-const reducer = (state, action) => {
+const reducer = (state = [], action) => {
+  console.log("previous-state", state );
   switch(action.type) {
     case 'add': 
-      console.log("add");
+      console.log("in add");
+      let newNote = {
+        id: uniqueNoteId++,
+        text: "",
+        completed: false
+      };
       return [
-        ...state, {
-          id: ++noteId,
-          text: "",
-          completed: false
-        } 
-      ]
+        ...state,
+        newNote
+    ]
     case 'update':
-      console.log("add");
+      console.log("in update");
       const todos = state.filter((todo) => {return todo.id !== action.id})
       return [
         ...todos,
@@ -31,18 +34,21 @@ const reducer = (state, action) => {
 
 const ToDo = () => {
   const [todos, dispatch] = React.useReducer(reducer, []);
+  console.log("updated-state", todos);
   return (
     <>
       <button onClick={() => {
+        console.log("onClick", uniqueNoteId);
         dispatch({type: 'add'})
       }}>Add</button>
       <div>
         {
         todos.map(({id, text, completed}) => {
+          console.log("id", id);
           return (
             <div>
-              <input type="checkbox" id="id" name="id"></input>
-              <input 
+              <input type="checkbox" id={id}></input>
+              {/* <input 
                 type="text" 
                 id={id}
                 name={id}
@@ -53,7 +59,7 @@ const ToDo = () => {
                   console.log("id", id);
                   dispatch({type: 'update', id, text: value})
                 }}
-              />
+              /> */}
             </div>
           )
         })
